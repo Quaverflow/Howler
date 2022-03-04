e72832648ca10746b7d0207929e26669f226ec06
Mirko Sangrigoli <mirko.sangrigoli@gmail.com>
2022-03-04 02:37:11 +0000

    working but slowish

---
+++ Howler.Tests/PerformanceTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Howler.Tests;
+
+public class PerformanceTest
+{
+    [Fact]
+    public void Test()
+    {
+        Thread.Sleep(100);
+        var stopwatch = new Stopwatch();
+        var howler = new Howler();
+
+        var iterations = 100;
+
+        stopwatch.Restart();
+
+        for (var i = 0; i < iterations; i++)
+        {
+          var p =  Hello();
+        }
+        var normal = stopwatch.ElapsedMilliseconds;
+
+        stopwatch.Restart();
+        for (var i = 0; i < iterations; i++)
+        {
+          var p = howler.Invoke(() => Hello());
+        }
+
+        var howl = stopwatch.ElapsedMilliseconds;
+        
+        Assert.True(true, $"normal{normal}, howler{howl}");
+    }
+
+
+    public string Hello()
+    {
+        var x = "hopera";
+        var p = 
+            x.Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a')
+            .Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a').Append('a');
+             return "hello" + p;
+    }
+}
--- Howler/Howler.cs
+++ Howler/Howler.cs
@@ -6,5 +6,4 @@ public class Howler : IHowler
 {
     public TResult Invoke<TResult>(Expression<Func<TResult>> original) => original.Compile().Invoke();
     public void InvokeVoid(Expression<Action> original) => original.Compile().Invoke();
-
 }
