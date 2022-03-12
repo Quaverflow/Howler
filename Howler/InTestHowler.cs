bae31185c8085e797139ad240214c13cd1ae5ed8
Mirko Sangrigoli <mirko.sangrigoli@gmail.com>
2022-03-12 11:21:46 +0000

    remove error

--- Howler/InTestHowler.cs
+++ Howler/InTestHowler.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics;
+
 namespace Howler;
 
 public class InTestHowler : IHowler
 {
-    public TResult Invoke<TResult>(Func<TResult> original, Guid? id = null) => original.Invoke();
     public TResult Invoke<TResult>(Func<TResult> original) => original.Invoke();
+
     public TResult Invoke<TResult>(Func<TResult> original, Guid id) => original.Invoke();
     public TResult InvokeGeneric<TData, TResult>(Func<TResult> original, Guid id, TData data) => original.Invoke();
     public TResult Invoke<TResult>(Func<TResult> original, Guid id, object data) => original.Invoke();
